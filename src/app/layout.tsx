c159271893adf53db65446f8312a3852c075502d
Guilherme <guiafsantos.dev@gmail.com>
2026-05-30 03:27:59 -0300

    scroll to top helper component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 import { Header } from '@/components/header';
+import ScrollToTop from '@/components/scroll-to-top';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.className} h-full antialiased`}>
       <body className="min-h-full">
+        <ScrollToTop />
         <Header />
         {children}
       </body>
