4602b9d1205c121fa0d5442777781b42808e1559
Guilherme <guiafsantos.dev@gmail.com>
2026-05-31 16:33:56 -0300

    home page redirects to /menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main>
-      <h1 className='text-2xl font-bold'>Web Menu</h1>
-    </main>
-  )
+  redirect('/menu');
 }
