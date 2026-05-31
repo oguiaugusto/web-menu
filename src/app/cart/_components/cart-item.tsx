dc89aefb0d46f995a815477b9c6ee37d45887cd7
Guilherme <guiafsantos.dev@gmail.com>
2026-05-31 20:47:48 -0300

    move cart page's items to separate component

---
+++ src/app/cart/_components/cart-item.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { Button } from '@/components/button';
+import { CartItem as CartItemType, useCart } from '@/providers/cart-provider';
+import { Minus, Plus, Trash2 } from 'lucide-react';
+
+type Props = {
+  item: CartItemType;
+};
+
+export function CartItem({ item }: Props) {
+  const { updateQuantity, removeItem } = useCart();
+
+  return (
+    <div key={item.id} className="flex gap-4 rounded-2xl border border-neutral-200 bg-white p-4">
+      <div className="relative h-20 w-20 shrink-0 overflow-hidden rounded-xl bg-neutral-200">
+        <img src="/example-image.png" alt="" className="h-full w-full object-cover" />
+        <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle,_transparent_55%,_rgba(0,0,0,0.45)_100%)]" />
+      </div>
+      <div className="flex flex-1 flex-col justify-between">
+        <div>
+          <h2 className="font-semibold">{item.name}</h2>
+          <p className="text-sm text-neutral-500">${item.price * item.quantity}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Button
+              variant="primary-outline"
+              className="flex h-8 w-8 items-center justify-center px-0 py-0"
+              onClick={() => updateQuantity(item.id, item.quantity - 1)}
+            >
+              <Minus size={16} />
+            </Button>
+            <span className="w-6 text-center font-medium">{item.quantity}</span>
+            <Button
+              variant="primary-outline"
+              className="flex aspect-square h-8 w-8 items-center justify-center px-0 py-0"
+              onClick={() => updateQuantity(item.id, item.quantity + 1)}
+            >
+              <Plus size={16} />
+            </Button>
+          </div>
+          <button
+            className="hover:text-red-muted active:text-red-muted-light cursor-pointer text-neutral-500"
+            onClick={() => removeItem(item.id)}
+          >
+            <Trash2 size={18} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import Link from 'next/link';
-import { Minus, Plus, Trash2 } from 'lucide-react';
 import { useCart } from '@/providers/cart-provider';
 import { Button } from '@/components/button';
 import { useRouter } from 'next/navigation';
 import { TEXT } from '@/constants/text';
+import { CartItem } from './_components/cart-item';
 
 export default function CartPage() {
   const router = useRouter();
-  const { items, total, updateQuantity, removeItem, clearCart } = useCart();
+  const { items, total, clearCart } = useCart();
 
   if (items.length === 0) {
     return (
@@ -26,7 +25,7 @@ export default function CartPage() {
   return (
     <main className="mx-auto max-w-4xl px-4 py-6">
       <div className="mb-6 flex items-end justify-between">
-        <h1 className="text-2xl font-bold">Your cart</h1>
+        <h1 className="text-2xl font-bold">{TEXT.yourCart}</h1>
         <button
           className="text-red-muted hover:text-red-muted-light cursor-pointer font-semibold active:brightness-106"
           onClick={() => clearCart()}
@@ -36,43 +35,7 @@ export default function CartPage() {
       </div>
       <div className="space-y-3">
         {items.map((item) => (
-          <div key={item.id} className="flex gap-4 rounded-2xl border border-neutral-200 bg-white p-4">
-            <div className="relative h-20 w-20 shrink-0 overflow-hidden rounded-xl bg-neutral-200">
-              <img src="/example-image.png" alt="" className="h-full w-full object-cover" />
-              <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle,_transparent_55%,_rgba(0,0,0,0.45)_100%)]" />
-            </div>
-            <div className="flex flex-1 flex-col justify-between">
-              <div>
-                <h2 className="font-semibold">{item.name}</h2>
-                <p className="text-sm text-neutral-500">${item.price * item.quantity}</p>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <Button
-                    variant="primary-outline"
-                    className="flex h-8 w-8 items-center justify-center px-0 py-0"
-                    onClick={() => updateQuantity(item.id, item.quantity - 1)}
-                  >
-                    <Minus size={16} />
-                  </Button>
-                  <span className="w-6 text-center font-medium">{item.quantity}</span>
-                  <Button
-                    variant="primary-outline"
-                    className="flex aspect-square h-8 w-8 items-center justify-center px-0 py-0"
-                    onClick={() => updateQuantity(item.id, item.quantity + 1)}
-                  >
-                    <Plus size={16} />
-                  </Button>
-                </div>
-                <button
-                  className="hover:text-red-muted active:text-red-muted-light cursor-pointer text-neutral-500"
-                  onClick={() => removeItem(item.id)}
-                >
-                  <Trash2 size={18} />
-                </button>
-              </div>
-            </div>
-          </div>
+          <CartItem key={`cart-item-${item.id}`} item={item} />
         ))}
       </div>
       <div className="sticky bottom-4 mt-6 rounded-2xl border border-neutral-200 bg-white p-4 shadow-lg">
