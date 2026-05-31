389c38b677597f727870cb5f3f9fefc645440fca
Guilherme <guiafsantos.dev@gmail.com>
2026-05-30 22:03:55 -0300

    install sonner & add toast after adding item to cart

--- src/app/menu/[id]/_components/quantity-and-add.tsx
+++ src/app/menu/[id]/_components/quantity-and-add.tsx
@@ -1,22 +1,39 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { toast } from 'sonner';
 import { TEXT } from '@/constants/text';
 import { QuantitySelector } from './quantity-selector';
 import { useCart } from '@/providers/cart-provider';
 import { MenuItem } from '@/data/menu-items';
+import { inter } from '@/constants/fonts';
 
 type Props = {
   data: MenuItem;
 };
 
 export function QuantityAndAdd({ data }: Props) {
+  const router = useRouter();
+
   const { addItem } = useCart();
   const [quantity, setQuantity] = useState(1);
 
   const handleAdd = () => {
     addItem({ id: data.id, name: data.name, price: data.price }, quantity);
     setQuantity(1);
+
+    toast.success(`${quantity}x ${data.name} ${TEXT.addedToCart}`, {
+      action: { label: 'Browse more items', onClick: () => router.push('/menu') },
+      duration: 4000,
+      position: 'bottom-center',
+      className: `${inter.className}`,
+      classNames: {
+        success: '!bg-red-muted !text-white',
+        icon: 'text-white',
+        actionButton: '!bg-white !text-red-muted',
+      },
+    });
   };
 
   return (
--- src/constants/text.ts
+++ src/constants/text.ts
@@ -2,4 +2,5 @@ export const TEXT = {
   menuTitle: 'Menu',
   menuSubtitle: 'Fresh food made to order',
   add: 'Add',
+  addedToCart: 'added to cart',
 };
