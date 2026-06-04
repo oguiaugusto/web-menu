8f06f16f19dbf4064082ced7620c5996bbd69432
Guilherme <guiafsantos.dev@gmail.com>
2026-06-04 16:45:55 -0300

    add prefix option to input ui component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,32 @@
+import { cn } from '@/utils/cn';
+
 type Props = Readonly<{
   label: string;
+  type?: HTMLInputElement['type'];
   name?: string;
   placeholder?: string;
   value?: string;
+  prefix?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }>;
 
-export function Input({ label, name, placeholder, value, onChange }: Props) {
+export function Input({ label, name, placeholder, value, prefix, onChange }: Props) {
   return (
     <label className="block space-y-1">
       <span className="text-sm font-medium">{label}</span>
-      <input
-        className="focus:border-red-muted shadow-red-muted/40 w-full rounded-lg border border-neutral-200 bg-white px-4 py-3 transition outline-none focus:shadow-xs"
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      <div className="relative">
+        {prefix ? <span className="absolute top-1/2 left-4 -translate-y-1/2">{prefix}</span> : null}
+        <input
+          className={cn(
+            'focus:border-red-muted shadow-red-muted/40 w-full rounded-lg border border-neutral-200 bg-white px-4 py-3 transition outline-none focus:shadow-xs',
+            prefix ? 'ps-7' : '',
+          )}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
     </label>
   );
 }
