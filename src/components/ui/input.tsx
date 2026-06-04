b70445bf6af14129f41e718bc77c89c4d0ef6257
Guilherme <guiafsantos.dev@gmail.com>
2026-06-04 17:13:45 -0300

    fix dynamic padding of input component when prefix exists

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { cn } from '@/utils/cn';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 type Props = Readonly<{
   label: string;
@@ -11,15 +14,30 @@ type Props = Readonly<{
 }>;
 
 export function Input({ label, name, placeholder, value, prefix, onChange }: Props) {
+  const prefixRef = useRef<HTMLElement>(null);
+  const [prefixWidth, setPrefixWidth] = useState<number>(0);
+
+  useLayoutEffect(() => {
+    if (prefixRef.current) {
+      setPrefixWidth(Math.ceil(prefixRef.current.getBoundingClientRect().width));
+    }
+  }, [prefix]);
+
   return (
     <label className="block space-y-1">
       <span className="text-sm font-medium">{label}</span>
       <div className="relative">
-        {prefix ? <span className="absolute top-1/2 left-4 -translate-y-1/2">{prefix}</span> : null}
+        {prefix ? (
+          <span className="absolute top-1/2 left-4 -translate-y-1/2" ref={prefixRef}>
+            {prefix}
+          </span>
+        ) : null}
         <input
+          style={{
+            paddingInlineStart: prefix ? `${18 + prefixWidth}px` : undefined,
+          }}
           className={cn(
             'focus:border-red-muted shadow-red-muted/40 w-full rounded-lg border border-neutral-200 bg-white px-4 py-3 transition outline-none focus:shadow-xs',
-            prefix ? 'ps-7' : '',
           )}
           name={name}
           placeholder={placeholder}
