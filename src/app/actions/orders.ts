068ebd5193b4a74c45c8849b18575feed8b95245
Guilherme <guiafsantos.dev@gmail.com>
2026-06-07 19:02:50 -0300

    createOrder action function

---
+++ src/app/actions/orders.ts
@@ -0,0 +1,137 @@
+'use server';
+
+import { Prisma } from '@/generated/prisma/client';
+import { prisma } from '@/lib/prisma';
+import { ErrorCode } from '@/types/enums';
+import { parseZodErrors } from '@/utils/parseZodErrors';
+import z from 'zod';
+
+const createOrderSchema = z.object({
+  customerName: z.string().nonempty(),
+  customerPhone: z.string().nonempty(),
+  deliveryAddress: z.string().nonempty(),
+  notes: z.string().optional(),
+  payment: z.string(),
+  changeFor: z.number().min(1).optional(),
+  items: z.array(
+    z.object({
+      id: z.string(),
+      quantity: z.number().int().positive(),
+    }),
+  ),
+});
+
+type CreateOrderInput = z.infer<typeof createOrderSchema>;
+
+type CreateOrderResult = { success: true } | { success: false; errors: ErrorType };
+
+type ItemInput = {
+  menuItemId: string;
+  name: string;
+  price: Prisma.Decimal;
+  quantity: number;
+};
+
+type ErrorType = {
+  fieldErrors: Record<string, { code: number }>;
+};
+
+function returnError(errors: ErrorType) {
+  return {
+    success: false,
+    errors,
+  };
+}
+
+function generateCode() {
+  const alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let code = 'WM-';
+
+  let index = 0;
+  while (index < 6) {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    code += alphabet[randomIndex];
+    index++;
+  }
+
+  return code;
+}
+
+export async function createOrder(rawData: CreateOrderInput): Promise<CreateOrderResult> {
+  const parsed = createOrderSchema.safeParse(rawData);
+  if (!parsed.success) {
+    return returnError({ fieldErrors: parseZodErrors(parsed.error) });
+  }
+
+  const { data } = parsed;
+
+  const menuItems = await prisma.menuItem.findMany({
+    where: { id: { in: data.items.map((x) => x.id) } },
+    select: { id: true, name: true, price: true },
+  });
+  const menuItemMap = new Map(menuItems.map((x) => [x.id, x]));
+
+  let total = new Prisma.Decimal(0);
+  const items: ItemInput[] = [];
+
+  for (const curr of data.items) {
+    const exItem = menuItemMap.get(curr.id);
+
+    if (!exItem) continue;
+
+    total = total.plus(exItem.price.mul(curr.quantity));
+
+    items.push({
+      menuItemId: curr.id,
+      name: exItem.name,
+      price: exItem.price,
+      quantity: curr.quantity,
+    });
+  }
+
+  if (items.length === 0) {
+    return returnError({
+      fieldErrors: {
+        items: {
+          code: ErrorCode.EMPTY_ORDER,
+        },
+      },
+    });
+  }
+
+  if (!['CASH', 'CARD'].includes(data.payment)) {
+    return returnError({ fieldErrors: { payment: { code: ErrorCode.INVALID_PAYMENT_METHOD } } });
+  }
+
+  if (data.changeFor !== undefined) {
+    if (data.payment !== 'CASH') {
+      data.changeFor = undefined;
+    } else {
+      if (new Prisma.Decimal(data.changeFor).lessThanOrEqualTo(total)) {
+        return returnError({ fieldErrors: { changeFor: { code: ErrorCode.CHANGE_FOR_BIGGER_THAN_TOTAL } } });
+      }
+    }
+  }
+
+  let code: string;
+
+  do {
+    code = generateCode();
+  } while (await prisma.order.findUnique({ where: { code } }));
+
+  await prisma.order.create({
+    data: {
+      code,
+      customerName: data.customerName,
+      customerPhone: data.customerPhone,
+      deliveryAddress: data.deliveryAddress,
+      notes: data.notes,
+      payment: data.payment as 'CASH' | 'CARD',
+      changeFor: data.changeFor,
+      total: total,
+      items: { createMany: { data: items } },
+    },
+  });
+
+  return { success: true };
+}
