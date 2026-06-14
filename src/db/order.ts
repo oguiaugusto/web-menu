f5bad678574d37fcac0e5c13b777a22ea32b0406
Guilherme <guiafsantos.dev@gmail.com>
2026-06-14 17:01:37 -0300

    fix getOrder total decimal conversion

--- src/db/order.ts
+++ src/db/order.ts
@@ -2,9 +2,10 @@ import { PAYMENT_METHODS } from '@/constants/text';
 import { OrderItem as PrismaOrderItem, Order as PrismaOrder } from '@/generated/prisma/client';
 import { prisma } from '@/lib/prisma';
 
-export type Order = Omit<PrismaOrder, 'changeFor' | 'payment'> & {
+export type Order = Omit<PrismaOrder, 'changeFor' | 'total' | 'payment'> & {
   changeFor: number | null;
-  payment: string,
+  total: number;
+  payment: string;
   items: (Omit<PrismaOrderItem, 'price'> & { price: number })[];
 };
 
@@ -19,6 +20,7 @@ export async function getOrder(code: string): Promise<Order | null> {
   return {
     ...order,
     changeFor: order.changeFor?.toNumber() ?? null,
+    total: order.total.toNumber(),
     payment: PAYMENT_METHODS[order.payment],
     items: order.items.map((x) => ({ ...x, price: x.price.toNumber() })),
   };
