942fd616347a4ecf2d893023691e5d2fd9daae09
Guilherme <guiafsantos.dev@gmail.com>
2026-08-14 21:51:28 -0300

    allowDevOrigins from env variable

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [process.env.DEV_IP ?? '']
 };
 
 export default nextConfig;
